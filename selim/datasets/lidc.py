3bf2afb6695d1edfb7abcd4cd22d781c8e72bb92
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-03-27 00:43:06 +0300

    minor

--- selim/datasets/lidc.py
+++ selim/datasets/lidc.py
@@ -11,6 +11,7 @@ def make_mask(image, image_id, nodules):
     height, width = image.shape
     nodule_image = np.zeros((height, width), np.uint8)
     # todo OR for all masks
+    edgeMap = None
     for nodule in nodules[:1]:
         print(nodule)
         for roi in nodule['roi']:
@@ -19,9 +20,13 @@ def make_mask(image, image_id, nodules):
                 edgeMap = roi['xy']
                 break
 
+    if edgeMap is None:
+        return image
+
     cv2.fillConvexPoly(nodule_image, np.array(edgeMap), (122, 122, 122))
     masked_data = cv2.bitwise_and(image, image, mask=nodule_image)
     cv2.imwrite('1.jpg', masked_data)
+    print("mask created\n")
     return masked_data
 
 
