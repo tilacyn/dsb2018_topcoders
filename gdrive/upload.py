b6cd46a28b7c3fe81eebbd838f1d974fe93f5ee7
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-03-28 00:34:16 +0300

    minor

--- gdrive/upload.py
+++ gdrive/upload.py
@@ -1,12 +1,10 @@
 from __future__ import print_function
-import pickle
+
+import os
 import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
-import os
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
