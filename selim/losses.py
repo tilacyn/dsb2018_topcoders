593ddabfe8f6d24305342715898439ffeb9620c9
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-03-29 21:56:46 +0300

    minor

--- selim/losses.py
+++ selim/losses.py
@@ -1,7 +1,10 @@
 import tensorflow.keras.backend as K
 from tensorflow.keras.losses import categorical_crossentropy
 import cv2
-from tensorflow import Tensor
+from tensorflow import convert_to_tensor
+import tensorflow as tf
+import math
+import numpy as np
 
 def hard_dice_coef(y_true, y_pred, smooth=1e-3):
     y_true_f = K.flatten(K.round(y_true[..., 0]))
@@ -33,7 +36,11 @@ def dice_coef_loss_bce(y_true, y_pred, dice=0.5, bce=0.5):
 
 
 def binary_crossentropy(y, p):
-    return K.mean(K.binary_crossentropy(y, p))
+    print(y.numpy())
+    print(p.numpy())
+    result = K.mean(K.binary_crossentropy(y, p))
+    print(result)
+    raise NotImplementedError
 
 
 def double_head_loss(y_true, y_pred):
@@ -50,7 +57,9 @@ def mask_contour_mask_loss(y_true, y_pred):
 
 
 def softmax_dice_loss(y_true, y_pred):
-    return categorical_crossentropy(y_true, y_pred) * 0.6 + dice_coef_loss(y_true[..., 0], y_pred[..., 0]) * 0.2 + dice_coef_loss(y_true[..., 1], y_pred[..., 1]) * 0.2
+    return categorical_crossentropy(y_true, y_pred) * 0.6 + dice_coef_loss(y_true[..., 0],
+                                                                           y_pred[..., 0]) * 0.2 + dice_coef_loss(
+        y_true[..., 1], y_pred[..., 1]) * 0.2
 
 
 def make_loss(loss_name):
@@ -73,7 +82,22 @@ def make_loss(loss_name):
     else:
         ValueError("Unknown loss.")
 
+
+def tsr(arr):
+    return convert_to_tensor(arr, dtype=tf.float32)
+
+
+def custom_bce(y_true, y_pred):
+    result = []
+    for i in range(len(y_pred)):
+        y_pred[i] = [max(min(x, 1 - K.epsilon()), K.epsilon()) for x in y_pred[i]]
+        result.append(-np.mean(
+            [y_true[i][j] * math.log(y_pred[i][j]) + (1 - y_true[i][j]) * math.log(1 - y_pred[i][j]) for j in
+             range(len(y_pred[i]))]))
+    return np.mean(result)
+
+
 def test():
     img1 = cv2.imread('/Users/mkryuchkov/lung-ds/000001.jpg')
     img2 = cv2.imread('/Users/mkryuchkov/lung-ds/000002.jpg')
-    return make_loss('bce_dice')(Tensor(img1), Tensor(img2))
+    return make_loss('bce_dice')(convert_to_tensor(img1, dtype=tf.float32), convert_to_tensor(img1, dtype=tf.float32))
